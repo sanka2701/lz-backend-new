0b2907c81d9fb1cb0879fcf00b6d4ab662cda2d3
sanka2701 <husenica.jan@gmail.com>
2018-03-22 20:22:13 +0100

    added file upload api

---
+++ src/main/java/sk/liptovzije/core/service/file/IStorageService.java
@@ -0,0 +1,21 @@
+package sk.liptovzije.core.service.file;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public interface IStorageService {
+    void init();
+
+    void store(MultipartFile file);
+
+    Stream<Path> loadAll();
+
+    Path load(String filename);
+
+    Resource loadAsResource(String filename);
+
+    void deleteAll();
+}
